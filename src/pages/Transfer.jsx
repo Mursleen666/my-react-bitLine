587d2d9340094e327ea2086bc5433aa0cce6e1c8
M.Mursleen <mursleenmamoon@gmail.com>
2025-07-15 19:56:59 +0500

    Updated

--- src/pages/Transfer.jsx
+++ src/pages/Transfer.jsx
@@ -85,7 +85,7 @@ const Transfer = () => {
             placeholder="Search..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="px-3 py-2 border border-gray-300 rounded-md w-[360px] lg:w-64"
+            className="px-3 py-2 border border-gray-300 rounded-md w-full lg:w-64"
           />
         </div>
       </div>
